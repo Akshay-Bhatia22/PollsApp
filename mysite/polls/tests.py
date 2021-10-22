d177df1387faca46f4d59b792a783b3927c607da
Akshay-bhatia22 <dpsvn.aksh06001@gmail.com>
2021-10-22 10:27:43 +0530

    testing

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+from .models import Question
 
-# Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        future_question = Question(pub_date=timezone.now()+datetime.timedelta(days=30))
+        self.assertIs(future_question.was_published_recently(), False)
+    
+    # should return false
+    def test_was_published_recently_with_past_questions(self):
+        past_question = Question(pub_date=timezone.now()-datetime.timedelta(days=2))
+        self.assertIs(past_question.was_published_recently(),False)
+    
+    def test_was_published_recently_with_recent_questions(self):
+        recent_question = Question(pub_date=timezone.now())
+        self.assertIs(recent_question.was_published_recently(),True)
+        
